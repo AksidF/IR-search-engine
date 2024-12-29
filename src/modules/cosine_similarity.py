30a3f76d12edef44d34036cabb7b98b02ffe3736
Putu Widyantara <putuwaw973@gmail.com>
2024-12-29 22:25:04 +0800

    feat: add search related features

---
+++ src/modules/cosine_similarity.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+
+def cosine_similarity(vec1, vec2):
+    vec1 = np.array(vec1)
+    vec2 = np.array(vec2)
+    dot_product = np.dot(vec1, vec2.T)
+    magnitude_vec1 = np.linalg.norm(vec1)
+    magnitude_vec2 = np.linalg.norm(vec2)
+
+    if magnitude_vec1 == 0 or magnitude_vec2 == 0:
+        return 0.0
+    similarity = dot_product / (magnitude_vec1 * magnitude_vec2)
+    return similarity
