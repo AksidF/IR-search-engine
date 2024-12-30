e90bc55b76a32ea4cda51223a970bd66cccfff9c
aksidf <diskafortunawan@gmail.com>
2024-12-30 08:59:47 +0800

    feat: create compression module

---
+++ src/compression.py
@@ -0,0 +1,39 @@
+from modules.dictionary import convert_list_to_str
+from modules.vb_encoding import save_vb_encoded_bits_to_file_multiline
+
+def compress_inverted_index(inverted_index):
+    """
+    Compress an inverted index by saving the dictionary string and VB-encoded indices to files.
+
+    Args:
+        inverted_index (dict): Inverted index with terms as keys and lists of doc_ids as values.
+    """
+
+    dictionary_file = "compressed/dictionary_string.txt"
+    vb_encoded_file = "compressed/vb_encoded.txt"
+
+    #Convert terms to a dictionary string format
+    terms = list(inverted_index.keys())
+    dictionary_string = convert_list_to_str(terms)
+
+    with open(dictionary_file, 'w') as file:
+        file.write(dictionary_string)
+    print(f"Dictionary string saved to {dictionary_file}.")
+
+    # Index lists for VB encoding
+    index_lists = list(inverted_index.values())
+    
+    save_vb_encoded_bits_to_file_multiline(index_lists, vb_encoded_file)
+    print(f"VB encoded data saved to {vb_encoded_file}. Compression completed.")
+
+# Example usage, delete later
+if __name__ == "__main__":
+    inverted_index = {
+        "Abidin": [1, 4, 9],
+        "Abilah": [2, 5, 8],
+        "Abimana": [3, 6, 7],
+        "Abing": [10, 12],
+        "Abiogenesis": [11, 13, 14, 15]
+    }
+
+    compress_inverted_index(inverted_index)
